2466e0ed6d3517b02da0499ef747c3424f056103
tkhamvilai <thanakornkhamvilai@gmail.com>
2019-12-10 21:25:17 -0500

    BenBenBen

--- tracker.py
+++ tracker.py
@@ -3,7 +3,70 @@ from cv2 import aruco
 import numpy as np
 import threading
 import queue
+import time
+import sys
 from mpi4py import MPI
+
+##############################################################################################################
+
+dt = 0.0015
+A = np.matrix('1 0 0; 0 1 0; 0 0 1') # stationary target
+B = np.matrix('0; 0; 0')
+Q = np.matrix('1 0; 0 1')
+xs = np.matrix('0; 0; 0')
+H = np.matrix('1 0 0; 0 1 0; 0 0 1')
+th = 0
+n = 2
+E = np.array([[0,1]])
+
+class Sensor_Init(object):
+    def __init__(self, ind, H, dt, A, B, Q, n, xs, th, E):
+        super(Sensor_Init, self).__init__()
+        self.xs = xs
+        self.P = np.matrix('1 0; 0 1')
+        self.z = np.matrix('0; 0')
+        self.th = th
+        self.u = np.matrix('0; 0')
+        self.dt = dt
+        self.A = A
+        self.B = B
+        self.Q = Q
+        self.H = H
+        self.R = 20*np.sqrt(ind+1)
+        self.U = 1/self.R * H.transpose() * H
+        self.x_bar = np.matrix(str(20*(np.random.random_sample() - 0.5)) + ';' + str(20*(np.random.random_sample() - 0.5))) + self.P*np.random.randn(2, 1)
+        self.message_out = {'u':1/self.R * H.transpose() * self.z, 'U':self.U, 'x_bar':self.x_bar}
+        self.message_in = [{'u':self.message_out['u'], 'U':self.message_out['U'], 'x_bar':self.message_out['x_bar']} for i in range(len(E))]
+        print(self.x_bar)
+
+def filter_update(s):
+    m = len(s.message_in) # number of neighbots
+    s.u = 1/s.R * s.H.transpose() * s.z # filtered measurement
+    y = s.u
+    S = s.U
+    w_sum = np.matrix('0; 0')
+    for i in range(m):
+        y = y + s.message_in[i]['u']
+        S = S + s.message_in[i]['U']
+        w_sum = w_sum + (s.message_in[i]['x_bar'] - s.x_bar)
+
+    M = np.linalg.inv(np.linalg.inv(s.P) + S)
+    x_hat = s.x_bar + M*(y - S*s.x_bar) + s.dt*M*w_sum # prediction
+#   print(str(rank) + ',' + str(x_hat)) 
+    
+    s.P = s.A*M*s.A + s.B*s.Q*s.B.transpose() # covariance update
+    s.x_bar = s.A*x_hat # corection
+    
+#   print(str(rank) + ',' + str(s.x_bar))   
+#   time.sleep(5)
+    s.message_out['u'] = s.u
+    s.message_out['U'] = s.U
+    s.message_out['x_bar'] = s.x_bar
+
+    return s
+
+
+##############################################################################################################
 
 def get_all_states(comm, state):
     size = comm.Get_size()
@@ -45,7 +108,7 @@ def aruco_tagger(frame_buffer,  aruco_dict, parameters, running):
             # cv.imshow('before_frame', current_frame)
             tagged_frame = current_frame.copy()
             tagged_frame = aruco.drawDetectedMarkers(tagged_frame, corners, ids)
-            cv.imshow('frame', tagged_frame)
+            # cv.imshow('frame', tagged_frame)
             cv.waitKey(1)
 
             return corners, ids
@@ -53,7 +116,8 @@ def aruco_tagger(frame_buffer,  aruco_dict, parameters, running):
 if __name__ == '__main__':
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
-    state = np.zeros(6, dtype = np.float) # [x, y, z, row, pitch, yaw]
+    # state = np.zeros(6, dtype = np.float) # [x, y, z, row, pitch, yaw]
+    state = np.zeros(3, dtype = np.float) # [x, y, yaw]
     cap = cv.VideoCapture(0)
 
     if not cap.isOpened():
@@ -82,21 +146,43 @@ if __name__ == '__main__':
     # while not displayed:
     while True:
         try:
+            start_time = time.time()
             aruco_corners, aruco_ids = aruco_tagger(frame_buffer, aruco_dict, aruco_parameters, aruco_tagger_running)
+            find_tags_time = time.time()
             # frame = frame_buffer.get_nowait()
             # https://www.docs.opencv.org/trunk/d9/d6a/group__aruco.html#ga84dd2e88f3e8c3255eb78e0f79571bd1
-            # rotations are in euler angles, translations in the same unit as tag_length
+            # rotations are in euler angles (rad), translations in the same unit as tag_length
             aruco_rotations, aruco_translations, object_points = aruco.estimatePoseSingleMarkers(aruco_corners, tag_length, cal_mtx, dist)
-            print(aruco_rotations)
+            pose_estimate_time = time.time()
+            # print("rot", aruco_rotations)
+            # print("trans", aruco_translations)
+            for i in range(0, len(state)):
+                # if i >= 0 and i < 3 and aruco_translations is not None:
+                #     state[i] = aruco_translations[0][0][i]
+                # elif i >= 3 and aruco_rotations is not None:
+                #     state[i] = aruco_rotations[0][0][i-3]
+                if i >= 0 and i < 2 and aruco_translations is not None:
+                    state[i] = aruco_translations[0][0][i]
+                elif i >= 2 and aruco_rotations is not None:
+                    state[i] = aruco_rotations[0][0][5]
+                else:
+                    state[i] = float('nan')
 
-            # example of how to update states
-            # for i in range(0, len(state)):
-            #     state[i] = rank + np.absolute(np.random.normal(loc = 0.0, scale = 0.1, size = 1))
+            sensor.z = state[i]
+            sensor = Sensor_Init(rank, H, dt, A, B, Q, n, xs, th, E)
+            sensor = filter_update(sensor)
 
             # call this when 'state' is updated
-            # all_states = get_all_states(comm, state)
+            all_states = get_all_states(comm, state)
+            comm_time = time.time()
             # print("my rank:", rank, ", my state:", state, "\nall states:", all_states)
-
+            if rank == 0:
+                print("all states:", all_states)
+                # print("find tags time:", find_tags_time - start_time)
+                # print("pose estimate time", pose_estimate_time - find_tags_time)
+                # print("comm time", comm_time - pose_estimate_time)
+                # print("total time", comm_time - start_time)
+            sys.stdout.flush()
             # displayed = True
         except queue.Empty:
             continue
